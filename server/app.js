a980f04f1bb4f549b0ca8208893d7b072a8c15e9
julia <juliahud@students.cs.ubc.ca>
2024-07-01 13:29:08 -0700

    Updated server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const port = 3001;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -16,5 +18,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../frontend/dist/index.html'));
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+});
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const readUsersFromFile = () => {
+  return JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json')));
+}
+
+let users = readUsersFromFile();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
 });
+
+router.get('/therapists', (req, res) => {
+  const therapists = fs.readFileSync(path.join(__dirname, '../data/therapists.json'));
+  res.send(therapists);
+});
+
+
 
 module.exports = router;
