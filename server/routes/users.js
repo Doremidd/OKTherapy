ad94dfbc60a9d93c4ec2001c9940fbb03dacb6c2
Doremidd <wangjyca@126.com>
2024-07-13 04:19:38 +0800

    bugs fixed2

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,14 @@
+
 var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 const path = require("path");
 const User = require("../models/userModel");
-const { user } = useAuth0();
 
 
 // GET a specific user
 router.get("/:userID", async function (req, res, next) {
-  const foundUser = await User.findOne({ userID: userID });
+  const foundUser = await User.findOne({ userID: req.params.userID });
   if (!foundUser) return res.status(404).send({ message: "User not found" });
   const userObject = {
     userID: foundUser.userID,
@@ -32,8 +32,9 @@ router.get("/:userID", async function (req, res, next) {
 // POST a user
 router.post("/", async function (req, res, next) {
   try {
-    const { userID, userName, userProfile } = req.body;
-    const newUser = new User({ userID, userName,userProfile });
+    const { userID, userName, profile } = req.body;
+    const newUser = new User({ userID, userName,profile });
+    console.log(newUser);
     await newUser.save();
     res.send(newUser);
   } catch (err) {
@@ -51,6 +52,7 @@ router.put("/:userID", async function (req, res, next) {
       { new: true }
     );
     if (!updatedUser) return res.status(404).send({ message: 'User not found' });
+    console.log(updatedUser);
     res.send(updatedUser);
   } catch (error) {
     next(error);
