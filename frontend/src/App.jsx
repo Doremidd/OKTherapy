2dbf34cb4c76080a31de4f8ff89ae402b1c00f07
isa-leroux448 <isa.leroux448@gmail.com>
2024-07-27 10:57:11 -0700

    redirect index.html to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Landing from './pages/Landing/Landing'
-import UserProfile from './pages/UserProfile/UserProfile'
-import Footer from './components/Footer'
-import EmailGenerator from './pages/EmailGenerator/EmailGenerator'
-import Home from './pages/Home'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Landing from "./pages/Landing/Landing";
+import UserProfile from "./pages/UserProfile/UserProfile";
+import Footer from "./components/Footer";
+import EmailGenerator from "./pages/EmailGenerator/EmailGenerator";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/index.html" element={<Navigate to="/home" replace />} />
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<UserProfile />} />
@@ -19,7 +20,7 @@ function App() {
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
