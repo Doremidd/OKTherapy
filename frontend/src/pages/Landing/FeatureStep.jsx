09a3d0a219b5e82c053d3cb5961b09eab49459af
isa-leroux448 <isa.leroux448@gmail.com>
2024-07-31 00:34:06 -0700

    add responsiveness to landing

--- frontend/src/pages/Landing/FeatureStep.jsx
+++ frontend/src/pages/Landing/FeatureStep.jsx
@@ -1,54 +1,54 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text } from "@chakra-ui/react";
 
 const FeatureStep = ({ number, title, description }) => {
-    return (
-        <Box
-            width={{ base: "100%", md: "85%" }}
-            height="260px"
-            padding="20px"
-            bg="#D5CFE1"
-            borderRadius="10px"
-            position="relative"
-        >
-            <Box
-                position="absolute"
-                top="10px"
-                left="10px"
-                width="40px"
-                height="40px"
-                bg="white"
-                borderRadius="50%"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                fontWeight="bold"
-                fontSize="20px"
-            >
-                {number}
-            </Box>
-            <Text
-                marginTop="40px"
-                fontStyle="normal"
-                fontWeight="600"
-                fontSize="24px"
-                lineHeight="32px"
-                color="#000000"
-            >
-                {title}
-            </Text>
-            <Text
-                marginTop="10px"
-                fontFamily="'Red Hat Text'"
-                fontStyle="normal"
-                fontWeight="400"
-                fontSize="18px"
-                lineHeight="28px"
-                color="#000000"
-            >
-                {description}
-            </Text>
-        </Box>
-    );
+  return (
+    <Box
+      width={{ base: "100%", md: "85%" }}
+      height={{ lg: "275px", md: "320px" }}
+      padding="20px"
+      bg="#D5CFE1"
+      borderRadius="10px"
+      position="relative"
+    >
+      <Box
+        position="absolute"
+        top="10px"
+        left="10px"
+        width="40px"
+        height="40px"
+        bg="white"
+        borderRadius="50%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        fontWeight="bold"
+        fontSize="20px"
+      >
+        {number}
+      </Box>
+      <Text
+        marginTop="40px"
+        fontStyle="normal"
+        fontWeight="600"
+        fontSize="24px"
+        lineHeight="32px"
+        color="#000000"
+      >
+        {title}
+      </Text>
+      <Text
+        marginTop="10px"
+        fontFamily="'Red Hat Text'"
+        fontStyle="normal"
+        fontWeight="400"
+        fontSize="18px"
+        lineHeight="28px"
+        color="#000000"
+      >
+        {description}
+      </Text>
+    </Box>
+  );
 };
 
 export default FeatureStep;
