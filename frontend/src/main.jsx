6648064390133b0cc9efc88b3502b9161b7ef027
isa-leroux448 <isa.leroux448@gmail.com>
2024-08-01 00:10:33 -0700

    add redux persistence

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import theme from "../theme.js";
 import store from "../store.js";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../store.js";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -14,17 +16,19 @@ const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <Auth0Provider
-          domain={domain}
-          clientId={clientId}
-          authorizationParams={{
-            redirect_uri: `${window.location.origin}/home`,
-          }}
-        >
-          <App />
-        </Auth0Provider>
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme}>
+          <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+              redirect_uri: `${window.location.origin}/home`,
+            }}
+          >
+            <App />
+          </Auth0Provider>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
