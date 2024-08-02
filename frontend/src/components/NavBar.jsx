510a52863e20043ff36cb2777fdede5e5cf87a9e
isa-leroux448 <isa.leroux448@gmail.com>
2024-08-01 22:20:02 -0700

    remove matches route

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,18 +3,21 @@ import "./style.css";
 import LoginButton from "./LoginButton";
 import DrawerMenu from "./DrawerMenu";
 
-const NavBar = () => {
+// eslint-disable-next-line react/prop-types
+const NavBar = ({ page }) => {
   let location = useLocation();
 
-  return (
-    <div
-      className="navbar"
-      style={{ background: location.pathname == "/" ? "#F0ECF7" : "none" }}
-    >
-      <DrawerMenu />
-      {location.pathname == "/" && <LoginButton />}
-    </div>
-  );
+  if (page !== "form" || location.pathname == "/") {
+    return (
+      <div
+        className="navbar"
+        style={{ background: location.pathname == "/" ? "#F0ECF7" : "none" }}
+      >
+        <DrawerMenu />
+        {location.pathname == "/" && <LoginButton />}
+      </div>
+    );
+  }
 };
 
 export default NavBar;
